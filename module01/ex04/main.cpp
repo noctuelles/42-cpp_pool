105c47e21c9a39410b5654f20e386f84c978c626
noctuelles <plouvel@42.student.fr>
2022-05-26 18:14:02 +0200

    added ex04

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouvel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 15:27:49 by plouvel           #+#    #+#             */
+/*   Updated: 2022/05/26 18:13:51 by plouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <errno.h>
+#include <string.h>
+#include <string>
+
+/* Sed for looser ! */
+
+int	main(int argc, char **argv)
+{
+	std::fstream	ifs,
+					ofs;
+	std::string		filename,
+					line,
+					str_to_replace,
+					by;
+
+	/* Checking arg counts. */
+
+	if (argc != 4)
+	{
+		std::cout << "Usage : " << argv[0] << " <filename> <str_to_replace> <by>" << std::endl;
+		return (1);
+	}
+	filename = argv[1];
+	str_to_replace = argv[2];
+	by = argv[3];
+
+	/* Opening files. */
+
+	ifs.open(argv[1], std::ofstream::in);
+	if (ifs.fail())
+	{
+		std::cout << "Can't open file '" << argv[1] << "': " << strerror(errno) << std::endl;
+		return (1);
+	}
+	ofs.open(filename.append(".replace").c_str(), std::ofstream::out);
+	if (ofs.fail())
+	{
+		std::cout << "Can't open file '" << argv[1] << "': " << strerror(errno) << std::endl;
+		return (1);
+	}
+
+	/* Read whole file. */
+
+	while (std::getline(ifs, line))
+	{
+		/* Find and replace. */
+		for (size_t i = 0; line[i]; i++)
+		{
+			if (line[i] == str_to_replace[0] && line.compare(i, str_to_replace.length(), str_to_replace) == 0)
+			{
+				ofs << by;
+				i += str_to_replace.length() - 1;
+			}
+			else
+				ofs << line[i];
+		}
+		/* Check if we haven't reached EOF to append a newline. */
+		if (std::cin.good())
+			ofs << std::endl;
+	}
+	ifs.close();
+	ofs.close();
+
+	return (0);
+}
---
+++ module01/ex04/tester.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Testing script for CPP Module 01 ex04
+
+PROGRAM="./sed_for_looser"
+
+echo -e "Opening non-existent file :\n"
+
+$PROGRAM foo a b
+echo
+
+echo -e "Opening non-readable file :\n"
+
+$PROGRAM ./tests/hello_world_not_readable a b
+echo
+
+echo -e "Replacing all space by '[#]' in ./tests/lorem_ipsum...\n"
+
+$PROGRAM ./tests/lorem_ipsum " " "[#]"
+diff --color ./tests/lorem_ipsum ./tests/lorem_ipsum.replace
+rm ./tests/lorem_ipsum.replace
+echo
+
+echo -e "Replacing the first paragraph by '_' ./tests/lorem_ipsum...\n"
+
+$PROGRAM ./tests/lorem_ipsum "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel eros porttitor, maximus ipsum et, scelerisque lectus. Nam tincidunt sed nisl ac iaculis. Vivamus nec blandit dui, vel rhoncus enim. Phasellus scelerisque diam dolor, id commodo neque tristique ac. Nunc cursus nisl quis erat condimentum, ac sagittis nisl scelerisque. Duis quis auctor ex, non vulputate erat. Nulla a imperdiet nisl, ultrices suscipit augue. Fusce molestie accumsan feugiat. Nullam vehicula feugiat lacus dignissim aliquam. Etiam sit amet dolor at neque blandit finibus quis eget felis. Morbi porta interdum eros, eget sollicitudin turpis varius ac." "_"
+diff --color ./tests/lorem_ipsum ./tests/lorem_ipsum.replace
+rm ./tests/lorem_ipsum.replace
+echo
+
+echo -e "Replacing nothing by '_' ./tests/lorem_ipsum...\n"
+
+$PROGRAM ./tests/lorem_ipsum "" "_"
+diff --color ./tests/lorem_ipsum ./tests/lorem_ipsum.replace
+rm ./tests/lorem_ipsum.replace
+echo
+
+echo -e "Replacing nothing by nothing ./tests/lorem_ipsum...\n"
+
+$PROGRAM ./tests/lorem_ipsum "" ""
+diff --color ./tests/lorem_ipsum ./tests/lorem_ipsum.replace
+rm ./tests/lorem_ipsum.replace
+echo
