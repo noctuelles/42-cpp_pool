3e9754184c20c44be715d6b4231bbeaa998f2dd5
noctuelles <plouvel@42.student.fr>
2022-05-26 20:01:23 +0200

    modification on ex05

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: plouvel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 19:09:30 by plouvel           #+#    #+#             */
-/*   Updated: 2022/05/26 19:34:14 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/05/26 20:00:03 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,19 @@ Harl::~Harl(void)
 void	Harl::_debug(void)
 {
 	std::cout << "[DEBUG]" << std::endl;
-	std::cout << "Mfff... would you mind knocking on the door next time ?" << std::endl;
+	std::cout << "Mfff... would you mind knocking on the door next time ?" << std::endl << std::endl;
 }
 
 void	Harl::_info(void)
 {
 	std::cout << "[INFO]" << std::endl;
-	std::cout << "What did you say ? Adding extra bacon costs extra money ? No way." << std::endl;
+	std::cout << "What did you say ? Adding extra bacon costs extra money ? No way." << std::endl << std::endl;
 }
 
 void	Harl::_warning(void)
 {
 	std::cout << "[WARNING]" << std::endl;
-	std::cout << "You better get out of the way ! I'm gonna find these bastard !" << std::endl;
+	std::cout << "You better get out of the way ! I'm gonna find these bastard !" << std::endl << std::endl;
 }
 
 void	Harl::_error(void)
