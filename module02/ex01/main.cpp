cd34ec2284a2b8207788b436a1483e2db382f004
noctuelles <plouvel@42.student.fr>
2022-05-27 18:49:10 +0200

    work of the day

--- module02/ex01/main.cpp
+++ module02/ex01/main.cpp
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plouvel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/27 17:05:21 by plouvel           #+#    #+#             */
-/*   Updated: 2022/05/27 18:48:52 by plouvel          ###   ########.fr       */
+/*   Created: 2022/05/27 17:56:21 by plouvel           #+#    #+#             */
+/*   Updated: 2022/05/27 18:00:13 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_CLASS_H
-# define FIXED_CLASS_H
+#include "Fixed.hpp"
+#include <iostream>
 
-class	Fixed
+int	main(void)
 {
-	public:
+	Fixed	a = Fixed();
+	Fixed	b = Fixed(a);
+	Fixed	c = Fixed();
 
-		Fixed(void);
-		Fixed(Fixed const & src);
-		~Fixed(void);
+	c = b;
 
-		Fixed & operator=(Fixed const & rhs); 
-
-		int		getRawBits(void);
-		void	setRawBits(int const raw);
-
-	private:
-
-		int					_n;
-		static int const	_nbrBits = 8;
-
-};
-
-#endif // FIXED_CLASS_H
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+}
